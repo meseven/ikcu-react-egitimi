0024287630aeede103fe0667c53357db5fb3666f
Mehmet Seven <mehmetseven0@gmail.com>
2020-12-09 16:17:45 +0300

    array state pushing

--- day1/3-react-base/src/App.js
+++ day1/3-react-base/src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 import React,{useState} from 'react' 
 
 // import Paragraf from './components/Paragraf';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
+import Users from './components/Users';
 
 function App() {
-	const [counterIsVisible, setCounterIsVisible] = useState(true);
+	// const [counterIsVisible, setCounterIsVisible] = useState(true);
 
 	return (
-		<div className="App">
+		<div>
 			{/* <Paragraf text='Ben ikinci paragrafım.' /> */}
 
-			{
+			{/* {
 				counterIsVisible && <Counter />
 			}
 
 			<input type="button" value="toggle" onClick={() => setCounterIsVisible(!counterIsVisible)} />
-			
+			 */}
+
+			 <Users />
 		</div>
 	);
 }
