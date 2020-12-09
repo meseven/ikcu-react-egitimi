3a90da1ab7eaf54dd737b75d281d61b9599821d7
Mehmet Seven <mehmetseven0@gmail.com>
2020-12-09 15:51:29 +0300

    react lifecycle methods

--- day1/3-react-base/src/App.js
+++ day1/3-react-base/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 
+import React,{useState} from 'react' 
+
 // import Paragraf from './components/Paragraf';
 import Counter from './components/Counter';
 
 function App() {
+	const [counterIsVisible, setCounterIsVisible] = useState(true);
+
 	return (
 		<div className="App">
 			{/* <Paragraf text='Ben ikinci paragrafım.' /> */}
 
-			<Counter />
+			{
+				counterIsVisible && <Counter />
+			}
+
+			<input type="button" value="toggle" onClick={() => setCounterIsVisible(!counterIsVisible)} />
+			
 		</div>
 	);
 }
--- day1/3-react-base/src/components/Counter.js
+++ day1/3-react-base/src/components/Counter.js
@@ -1,23 +1,46 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Counter() {
+	const [user, setUser] = useState({ name: 'burak', age: 29 });
+
 	const [count, setCount] = useState(0);
 
 	const artir = () => setCount(count + 1);
-	
 	const azalt = () => {
 		if (count > 0) {
-			setCount(count - 1)
+			setCount(count - 1);
 		}
 	};
-	
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCount((count) => count + 1);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	useEffect(() => {
+		console.log('count state değişti');
+	}, [count]);
+
 	return (
 		<div>
 			<h1>{count}</h1>
 			<input type="button" value="-" onClick={azalt} disabled={count < 1} />
 			<input type="button" value="+" onClick={artir} />
+			<hr />
+			<hr />
+			<hr />
+			Name: {user.name} <br />
+			Age: {user.age} <br />
+			<input
+				type="button"
+				value="Change user state"
+				onClick={() => setUser({ ...user, name: 'Mehmet' })}
+			/>
 		</div>
-	)
+	);
 }
 
-export default Counter
+export default Counter;
