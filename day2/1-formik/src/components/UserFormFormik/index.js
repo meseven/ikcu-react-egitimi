bc00073ff719ddec55099d1431d8575714eb4727
Mehmet Seven <mehmetseven0@gmail.com>
2020-12-10 11:36:12 +0300

    Formik validations with yup

--- day2/1-formik/src/components/UserFormFormik/index.js
+++ day2/1-formik/src/components/UserFormFormik/index.js
@@ -1,5 +1,8 @@
 import { Formik } from "formik";
 
+import validations from "./validations";
+import styles from "./styles.module.css";
+
 function UserForm() {
 	return (
 		<>
@@ -10,22 +13,37 @@ function UserForm() {
 					passwordConfirm: "",
 				}}
 				onSubmit={async (values, bag) => {
-					// console.log(bag);
+					console.log(bag);
 					await new Promise((r) => setTimeout(r, 2000));
 					bag.resetForm();
 					console.log(values);
 				}}
+				validationSchema={validations}
 			>
-				{({ values, handleChange, handleSubmit, isSubmitting }) => (
+				{({
+					values,
+					handleChange,
+					handleSubmit,
+					handleBlur,
+					isSubmitting,
+					errors,
+					touched,
+				}) => (
 					<div>
+						<div style={{ fontSize: 11 }}>{JSON.stringify(errors)}</div>
 						<div>
 							<input
 								placeholder="e-mail"
 								name="email"
 								value={values.email}
 								onChange={handleChange}
+								onBlur={handleBlur}
 								disabled={isSubmitting}
 							/>
+							{errors.email && touched.email && (
+								<div className={styles.error}>{errors.email}</div>
+							)}
+							{errors.email ? "" : ""}
 						</div>
 						<div>
 							<input
@@ -33,8 +51,12 @@ function UserForm() {
 								name="password"
 								value={values.password}
 								onChange={handleChange}
+								onBlur={handleBlur}
 								disabled={isSubmitting}
 							/>
+							{errors.password && touched.password && (
+								<div className={styles.error}>{errors.password}</div>
+							)}
 						</div>
 						<div>
 							<input
@@ -42,8 +64,12 @@ function UserForm() {
 								name="passwordConfirm"
 								value={values.passwordConfirm}
 								onChange={handleChange}
+								onBlur={handleBlur}
 								disabled={isSubmitting}
 							/>
+							{errors.passwordConfirm && touched.passwordConfirm && (
+								<div className={styles.error}>{errors.passwordConfirm}</div>
+							)}
 						</div>
 						<div>
 							<input
