c8ea1c385e9ab65a7746521c0b153650a0d926a0
Mehmet Seven <mehmetseven0@gmail.com>
2020-12-13 14:14:46 +0300

    react navigation drawer

--- day5/NavigationApp/App.js
+++ day5/NavigationApp/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 // screens
 import HomeScreen from './src/screens/Home';
@@ -11,12 +15,45 @@ import CommentDetailScreen from './src/screens/CommentDetail';
 //components
 import HeaderLogo from './src/components/HeaderLogo';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
+// const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 const App = () => {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator
+			<Drawer.Navigator initialRouteName="Notifications">
+				<Drawer.Screen name="Home" component={HomeScreen} />
+				<Drawer.Screen name="Comments" component={CommentsScreen} />
+			</Drawer.Navigator>
+
+			{/* <Tab.Navigator
+				screenOptions={({route}) => ({
+					tabBarIcon: ({focused, color, size}) => {
+						let iconName;
+
+						if (route.name === 'Home') {
+							iconName = 'home';
+						} else if (route.name === 'Comments') {
+							iconName = 'comments';
+						} else if (route.name === 'Settings') {
+							iconName = 'cog';
+						}
+
+						// You can return any component that you like here!
+						return <Icon name={iconName} size={size} color={color} />;
+					},
+				})}
+				tabBarOptions={{
+					activeTintColor: '#333',
+					inactiveTintColor: '#ddd',
+				}}>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Comments" component={CommentsScreen} />
+				<Tab.Screen name="Settings" component={CommentsScreen} />
+			</Tab.Navigator> */}
+
+			{/* <Stack.Navigator
 				screenOptions={{
 					headerStyle: {
 						backgroundColor: '#f4511e',
@@ -42,7 +79,7 @@ const App = () => {
 						headerBackTitle: ' ',
 					})}
 				/>
-			</Stack.Navigator>
+			</Stack.Navigator> */}
 		</NavigationContainer>
 	);
 };
--- day5/NavigationApp/src/screens/Home/index.js
+++ day5/NavigationApp/src/screens/Home/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
 
 import LottieView from 'lottie-react-native';
+
+import {Button, Text} from 'native-base';
 
 const Home = ({navigation}) => {
 	return (
 		<View style={styles.container}>
-			<TouchableOpacity onPress={() => navigation.navigate('Comments')}>
-				<Text style={styles.btn}>Go to Comments</Text>
-			</TouchableOpacity>
+			<View style={{paddingVertical: 40, flex: 1}}>
+				<Button block onPress={() => navigation.navigate('Comments')}>
+					<Text>Go to Comments</Text>
+				</Button>
+			</View>
 
 			<LottieView
 				style={{width: 300}}
@@ -25,8 +29,7 @@ export default Home;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'space-between',
+		justifyContent: 'center',
 	},
 	btn: {
 		fontSize: 22,
